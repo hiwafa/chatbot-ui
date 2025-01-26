6e77f8d7872e322845a4d9bea7bb551db4e35fee
Muhammad Wafa <mq.func@gmail.com>
2025-01-25 18:37:50 -0800

    update user

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,22 +1,26 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, TextInput, Button, FlatList, Image, StyleSheet, TouchableOpacity, Keyboard, Modal, TouchableWithoutFeedback } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, FlatList, Image, StyleSheet, TouchableOpacity, Modal } from 'react-native';
+import { RadioButton } from 'react-native-paper';
 import axios from 'axios';
 import { useConfirmationDialog } from '@/src/components/ConfirmationDialog';
 
 const apiUrl = "http://127.0.0.1:8000";
 
 export default function App() {
-  
+
   const confirmDialog = useConfirmationDialog();
-  
+
   const [users, setUsers] = useState([]);
   const [userId, setUserId] = useState('');
   const [userFirstName, setUserFirstName] = useState('');
   const [userLastName, setUserLastName] = useState('');
   const [userAbout, setUserAbout] = useState('');
   const [userImage, setUserImage] = useState('');
+  const [selectedRole, setSelectedRole] = useState<string>('Viewer');
   const [userDateOfBirth, setUserDateOfBirth] = useState('');
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [currentUserDBId, setCurrentUserDBId] = useState(null);
+
 
   // Fetch users from the backend
   const fetchUsers = async () => {
@@ -37,6 +41,7 @@ export default function App() {
     try {
       const response = await axios.post(`${apiUrl}/add_user`, {
         user_id: userId,
+        user_role: selectedRole,
         user_first_name: userFirstName,
         user_last_name: userLastName,
         user_about: userAbout,
@@ -44,36 +49,71 @@ export default function App() {
         user_date_of_birth: userDateOfBirth,
       });
       setIsModalVisible(false);
-      fetchUsers(); 
+      fetchUsers();
     } catch (error) {
       console.error('Error adding user:', error);
     }
-  };
+  }
+
+  const updateUser = async () => {
+    try {
+
+      console.log("user_db_id: ", currentUserDBId)
+      // const queryParams = new URLSearchParams({
+      //   _id: currentUserDBId ? currentUserDBId : "",
+      //   user_id: userId,
+      //   user_role: selectedRole,
+      //   user_first_name: userFirstName,
+      //   user_last_name: userLastName,
+      //   user_about: userAbout,
+      //   user_image: userImage,
+      //   user_date_of_birth: userDateOfBirth,
+      // }).toString();
+  
+      const response = await axios.put(`${apiUrl}/edit_user`, {
+        _id: currentUserDBId ? currentUserDBId : "",
+        user_id: userId,
+        user_role: selectedRole,
+        user_first_name: userFirstName,
+        user_last_name: userLastName,
+        user_about: userAbout,
+        user_image: userImage,
+        user_date_of_birth: userDateOfBirth,
+      });
+
+      setIsModalVisible(false);
+      setCurrentUserDBId(null);
+      fetchUsers();
+
+    } catch (error) {
+      console.error('Error updating user:', error);
+    }
+  }
+
 
   // Delete a user
   const deleteUser = async (id) => {
     try {
 
       const result = await confirmDialog({
-        message: 'Do you want to proceed?',
+        message: 'Do you want to delete this record?',
         confirmText: 'Yes',
         cancelText: 'No',
       });
-  
+
       if (result) {
-        console.log('User confirmed!');
+        await axios.delete(`${apiUrl}/delete_user?user_id=${id}`);
+        fetchUsers();
       } else {
         console.log('User canceled!');
       }
 
-      // await axios.delete(`${apiUrl}/delete_user?user_id=${id}`);
-      fetchUsers();
     } catch (error) {
       console.error('Error deleting user:', error);
     }
   };
 
-  const emptyModalTextFields = ()=> {
+  const emptyModalTextFields = () => {
     setUserId("");
     setUserFirstName("");
     setUserLastName("");
@@ -81,8 +121,9 @@ export default function App() {
     setUserImage("");
     setUserDateOfBirth("");
   }
-  const setModalTextFields = ({user_id, user_first_name, user_last_name, user_about, user_image, user_date_of_birth})=> {
+  const setModalTextFields = ({ user_id, user_role, user_first_name, user_last_name, user_about, user_image, user_date_of_birth }) => {
     setUserId(user_id);
+    setSelectedRole(user_role);
     setUserFirstName(user_first_name);
     setUserLastName(user_last_name);
     setUserAbout(user_about);
@@ -90,10 +131,11 @@ export default function App() {
     setUserDateOfBirth(user_date_of_birth);
   }
 
-  // Edit a user (this can be expanded to show a modal for editing)
+  // Edit a user 
   const editUser = (user) => {
     setIsModalVisible(true);
-    setModalTextFields(user)
+    setModalTextFields(user);
+    setCurrentUserDBId(user._id)
   }
 
   const renderItem = ({ item }) => (
@@ -117,10 +159,15 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button title="Add User" onPress={() => {
-        setIsModalVisible(true);
-        emptyModalTextFields();
-      }} style={styles.addButton} />
+
+      <TouchableOpacity style={[styles.button, { backgroundColor: '#28a745', maxWidth: 150, paddingHorizontal: 10, marginRight: 5, alignSelf: 'flex-end' }]}
+        onPress={() => {
+          setIsModalVisible(true);
+          emptyModalTextFields();
+        }}>
+        <Text style={styles.buttonText}>+ Add User</Text>
+      </TouchableOpacity>
+
       <FlatList
         data={users}
         renderItem={renderItem}
@@ -131,54 +178,77 @@ export default function App() {
         animationType="slide"
         transparent={true}
         visible={isModalVisible}
-        onRequestClose={() => setIsModalVisible(false)}
+        onRequestClose={() => {
+          setIsModalVisible(false);
+          setCurrentUserDBId(null);
+        }}
       >
         {/* <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}> */}
-          <View style={styles.modalContainer}>
-            <View style={styles.modalContent}>
-              <Text style={styles.modalTitle}>Add New User</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="User ID"
-                value={userId}
-                onChangeText={setUserId}  // Ensure this is updating the state
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="First Name"
-                value={userFirstName}
-                onChangeText={setUserFirstName}  // Ensure this is updating the state
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Last Name"
-                value={userLastName}
-                onChangeText={setUserLastName}  // Ensure this is updating the state
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="About"
-                value={userAbout}
-                onChangeText={setUserAbout}  // Ensure this is updating the state
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Image URL"
-                value={userImage}
-                onChangeText={setUserImage}  // Ensure this is updating the state
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Date of Birth"
-                value={userDateOfBirth}
-                onChangeText={setUserDateOfBirth}  // Ensure this is updating the state
-              />
-              <View style={styles.modalButtons}>
-                <Button title="Add User" onPress={addUser} />
-                <Button title="Cancel" onPress={() => setIsModalVisible(false)} />
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Add New User</Text>
+            <RadioButton.Group onValueChange={newValue => setSelectedRole(newValue)} value={selectedRole}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+                <Text style={{ fontWeight: 'bold' }}>User Role:</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <RadioButton value="manager" />
+                  <Text>Manager</Text>
+                </View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <RadioButton value="editor" />
+                  <Text>Editor</Text>
+                </View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <RadioButton value="viewer" />
+                  <Text>Viewer</Text>
+                </View>
               </View>
+            </RadioButton.Group>
+            <TextInput
+              style={styles.input}
+              placeholder="User ID"
+              value={userId}
+              onChangeText={setUserId}  // Ensure this is updating the state
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="First Name"
+              value={userFirstName}
+              onChangeText={setUserFirstName}  // Ensure this is updating the state
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Last Name"
+              value={userLastName}
+              onChangeText={setUserLastName}  // Ensure this is updating the state
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="About"
+              value={userAbout}
+              onChangeText={setUserAbout}  // Ensure this is updating the state
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Image URL"
+              value={userImage}
+              onChangeText={setUserImage}  // Ensure this is updating the state
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Date of Birth"
+              value={userDateOfBirth}
+              onChangeText={setUserDateOfBirth}  // Ensure this is updating the state
+            />
+            <View style={styles.modalButtons}>
+              <Button title={currentUserDBId ? "Update User" : "+ Add User"} onPress={currentUserDBId ? updateUser : addUser} />
+              <Button title="Cancel" onPress={() => {
+                setIsModalVisible(false);
+                setCurrentUserDBId(null);
+              }} />
             </View>
           </View>
+        </View>
         {/* </TouchableWithoutFeedback> */}
       </Modal>
     </View>
@@ -191,11 +261,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#f0f0f0',
     paddingTop: 20,
     paddingHorizontal: 15,
-  },
-  addButton: {
-    marginBottom: 20,
-    backgroundColor: '#4CAF50',
-    color: '#fff',
   },
   userCard: {
     flexDirection: 'row',
@@ -276,5 +341,77 @@ const styles = StyleSheet.create({
   modalButtons: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+    elevation: 3, // For Android shadow effect
+    margin: 5
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  radioGroup: {
+    marginBottom: 20,
+  },
+  radioButtonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#333',
+  },
+  radioCircle: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 10,
+  },
+  selectedCircle: {
+    height: 10,
+    width: 10,
+    borderRadius: 5,
+    backgroundColor: '#00cc99',
+  },
+  radioTextContainer: {
+    flex: 1,
+  },
+  radioLabel: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: '600',
+  },
+  radioDescription: {
+    fontSize: 14,
+    color: '#aaa',
+    marginTop: 2,
+  },
+  resultContainer: {
+    marginTop: 20,
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: '#2c2c2c',
+    alignItems: 'center',
+  },
+  resultText: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#fff',
   },
 });
